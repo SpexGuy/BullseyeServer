b53249665c963d1ca3f8e1b0edc7948128c0e4e3
Ben Behar <behar006@umn.edu>
2014-09-27 02:59:56 -0500

    added skeleton for 'getAisleOfScan'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var Bullseye = require('./Bullseye.js');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
