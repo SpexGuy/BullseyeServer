7b332ce787029603c1c5e89f9f7e77c5537e2212
Martin Wickham <spexguy070@gmail.com>
2014-09-27 11:30:12 -0500

    Added money to upgrades

--- Player.js
+++ Player.js
@@ -108,6 +108,7 @@ module.exports = function(username) {
 
 		getUpgrades: function() {
 			return {
+				money: this.money,
 				available: this.upgradesAvailable,
 				unlocked: this.upgradesUnlocked,
 				inProgress: this.upgradesInProgress
